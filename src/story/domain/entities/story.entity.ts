b30fe856a1af43c0cd5a6428ccf2907624c12d63
Liang Camilo <liangcamilo2006@gmail.com>
2026-04-13 23:19:28 -0500

    fix: changed how story entity internals works

--- src/story/domain/entities/story.entity.ts
+++ src/story/domain/entities/story.entity.ts
@@ -1,19 +1,41 @@
 import { StoryDescription } from '../value-objects/story-vo/story-description.vo';
+import { StoryId } from '../value-objects/story-vo/story-id.vo';
 import { StoryTitle } from '../value-objects/story-vo/story-title.vo';
 import { Chapter } from './chapter.entity';
 import { Rating } from './rating.entity';
-import { Tag } from './tag.entity';
 
 export class Story {
   constructor(
-    private id: string,
+    private id: StoryId,
     private title: StoryTitle,
     private description: StoryDescription,
-    private ratings: Rating[] = [],
+    private hidden: boolean = true,
     private userId: string,
     private genreId: string,
     private chapters: Chapter[] = [],
-    private tags: Tag[] = [],
+    private tagIds: string[] = [],
     private subgenreId?: string,
   ) {}
+
+  static create(params: {
+    title: string;
+    description: string;
+    hidden: boolean;
+    userId: string;
+    genreId: string;
+    subgenreId?: string;
+    tagIds?: string[];
+  }): Story {
+    return new Story(
+      new StoryId(),
+      new StoryTitle(params.title),
+      new StoryDescription(params.description),
+      params.hidden,
+      params.userId,
+      params.genreId,
+      [], // chapters
+      params.tagIds,
+      params.subgenreId,
+    );
+  }
 }
