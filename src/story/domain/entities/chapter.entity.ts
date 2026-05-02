f4f06d2a0a99a8accbb7562fc8034f408bef0db4
Liang Camilo <liangcamilo2006@gmail.com>
2026-05-01 19:14:43 -0500

    feat: added getters and to primitives to entity

--- src/story/domain/entities/chapter.entity.ts
+++ src/story/domain/entities/chapter.entity.ts
@@ -1,19 +1,58 @@
 import { ChapterOrder } from '../value-objects/chapter-vo/chapter-order.vo';
 import { ChapterTitle } from '../value-objects/chapter-vo/chapter-title.vo';
+import { Id } from '../value-objects/id.vo';
 
 export class Chapter {
   constructor(
-    private id: string,
+    private id: Id,
     private title: ChapterTitle,
     private order: ChapterOrder,
     private storyId: string,
     private content: string,
   ) {}
 
-  // static create(params: {
-  //   title: string;
-  //   order: Number;
-  //   storyId: string
-  //   paragraphs?:
-  // })
+  static create(params: {
+    title: string;
+    order: number;
+    storyId: string;
+    content: string;
+  }) {
+    return new Chapter(
+      new Id(params.storyId),
+      new ChapterTitle(params.title),
+      new ChapterOrder(params.order),
+      params.storyId,
+      params.content,
+    );
+  }
+
+  get getId() {
+    return this.id;
+  }
+
+  get getTitle() {
+    return this.title;
+  }
+
+  get getStoryId() {
+    return this.storyId;
+  }
+
+  get getOrder() {
+    return this.order;
+  }
+
+  get getContent() {
+    return this.content;
+  }
+
+  toPrimitives() {
+    return {
+      id: this.getId.getValue,
+      title: this.title.getValue,
+      content: this.content,
+      storyId: this.storyId,
+      order: this.order.getValue,
+    };
+  }
 }
