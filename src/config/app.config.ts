7fa8dc23b17bc42ccf1a87a7ab9b5f1b47cbb419
Liang Camilo <liangcamilo2006@gmail.com>
2026-05-05 22:50:32 -0500

    removed frontend_url for app.config

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -2,5 +2,4 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('app', () => ({
   port: parseInt(process.env.PORT ?? '3000'),
-  frontendUrl: process.env.FRONTEND_URL ?? 'http://localhost:5173',
 }));
