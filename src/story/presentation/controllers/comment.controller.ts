73a04b3eb90ea65a7905f13da5148bc8b4709a4f
Liang Camilo <liangcamilo2006@gmail.com>
2026-05-10 18:36:27 -0500

    fix: added Post decorator to create method in controller

--- src/story/presentation/controllers/comment.controller.ts
+++ src/story/presentation/controllers/comment.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { CreateCommentDto } from 'src/story/application/dtos/comment-dtos/create-comment.dto';
 import { CreateCommentUseCase } from 'src/story/application/use-cases/comment-use-cases/create-comment.use-case';
 
@@ -6,6 +6,7 @@ import { CreateCommentUseCase } from 'src/story/application/use-cases/comment-us
 export class CommentController {
   constructor(private createCommentUseCase: CreateCommentUseCase) {}
 
+  @Post('create')
   async create(@Body() createCommentDto: CreateCommentDto) {
     const newComment =
       await this.createCommentUseCase.execute(createCommentDto);
